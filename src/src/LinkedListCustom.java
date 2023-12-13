3fa7ed5a2ca35dc58c04e8441502cc4036ead63a
Aydar.Fattakhov <fat.aid@mail.ru>
2023-12-13 18:56:07 +0300

    test

---
+++ src/src/LinkedListCustom.java
@@ -0,0 +1,14 @@
+
+import java.io.*;
+import java.util.LinkedList;
+
+public class LinkedListCustom {
+    Node head;
+
+    /*public LinkedListCustom add(LinkedListCustom list, E data){
+        Node new_node = new Node(data);
+
+    }*/
+
+}
+
---
+++ src/src/Node.java
@@ -0,0 +1,8 @@
+public class Node<E> {
+    E data;
+    Node<E> next;
+    Node(E d){
+        data = d;
+        next = null;
+    }
+}
